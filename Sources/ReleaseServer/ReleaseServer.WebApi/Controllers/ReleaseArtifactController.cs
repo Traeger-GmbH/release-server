3050132837658799e383ea5205f73d8ec0eea306
Timo Walter <walter.timoand@gmail.com>
2020-03-14 12:13:00 +0100

    #42 Add an XML documentation to the Controller operations

--- Sources/ReleaseServer/ReleaseServer.WebApi/Controllers/ReleaseArtifactController.cs
+++ Sources/ReleaseServer/ReleaseServer.WebApi/Controllers/ReleaseArtifactController.cs
@@ -1,7 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
@@ -30,7 +29,20 @@ namespace ReleaseServer.WebApi.Controllers
             ReleaseArtifactService = releaseArtifactService;
         }
         
+        /// <summary>
+        /// Uploads a specific release artifact.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="os"></param>
+        /// <param name="architecture"></param>
+        /// <param name="version"></param>
+        /// <response code="200">Upload of the artifact successful.</response>
+        /// <response code="401">The user is not authorized (wrong credentials or missing auth header.</response>
+        /// <response code="500">Wrong body provided (not 'content-type: multipart/form-data').</response>
         [HttpPut("upload/{product}/{os}/{architecture}/{version}")]
+        [ProducesResponseType(typeof(IActionResult), 200)]
+        [ProducesResponseType(typeof(void), 401)]
+        [ProducesResponseType(typeof(void), 500)]
         //Max. 500 MB
         [RequestSizeLimit(524288000)]
         public IActionResult UploadSpecificArtifact([Required] string product, [Required] string os, [Required] string architecture, [Required] string version)
@@ -45,14 +57,33 @@ namespace ReleaseServer.WebApi.Controllers
             return Ok("Upload of the artifact successful!");
         }
         
+        /// <summary>
+        /// Retrieves all available products.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <response code="200">A product with the specified product name exists.
+        /// Empty productInformation, if there exists no product with the specified product name</response>
+        /// <response code="500">Wrong body provided (not 'content-type: multipart/form-data').</response>
+        /// <returns></returns>
         [AllowAnonymous]
+        [ProducesResponseType(typeof(ProductInformationListResponseModel), 200)]
+        [ProducesResponseType(typeof(void), 500)]
         [HttpGet("versions/{product}")]
         public ProductInformationListResponseModel GetProductInfos([Required] string product)
         {
             return ReleaseArtifactService.GetProductInfos(product).ToProductInfoListResponse();
         }
 
+        /// <summary>
+        /// Retrieves all available platforms for a specific product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="version"></param>
+        /// <response code="200">There are existing Platforms for the specified product name. Empty platforms array, if
+        /// there exists no platforms for the specified product name</response>
+        /// <returns></returns>
         [AllowAnonymous]
+        [ProducesResponseType(typeof(PlatformsResponseModel), 200)]
         [HttpGet("platforms/{product}/{version}")]
         public PlatformsResponseModel GetPlatforms([Required] string product, [Required]string version)
         {
@@ -61,22 +92,60 @@ namespace ReleaseServer.WebApi.Controllers
             return platformsList.ToPlatformsResponse();
         }
         
+        /// <summary>
+        /// Retrieves the changelog of a specific product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="os"></param>
+        /// <param name="architecture"></param>
+        /// <param name="version"></param>
+        /// <response code="200">The specific product exists.</response>
+        /// <response code="500">The product with the specified product name does not exist
+        /// or there is no changelog available.</response>
+        /// <returns></returns>
         [AllowAnonymous]
+        [ProducesResponseType(typeof(ChangelogResponseModel), 200)]
+        [ProducesResponseType(typeof(void), 500)]
         [HttpGet("info/{product}/{os}/{architecture}/{version}")]
         public ChangelogResponseModel GetReleaseInfo([Required] string product, [Required] string os, [Required] string architecture, [Required] string version)
         {
             return ReleaseArtifactService.GetReleaseInfo(product, os, architecture, version).toChangelogResponse();
         }
         
+        /// <summary>
+        /// Retrieves all available versions that are fitting to a specific product / platform (HW architecture + OS).
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="os"></param>
+        /// <param name="architecture"></param>
+        /// <response code="200">There are existing versions for the specified platform and product.
+        /// Empty versions array, if there exists no version for the specified platform / product.</response>
+        /// <response code="500">There exists no product with the specified product name.</response>
+        /// <returns></returns>
         [AllowAnonymous]
+        [ProducesResponseType(typeof(ProductVersionListResponseModel), 200)]
+        [ProducesResponseType(typeof(string), 500)]
         [HttpGet("versions/{product}/{os}/{architecture}")]
         public ProductVersionListResponseModel GetVersions([Required] string product, [Required] string os, [Required] string architecture)
         {
             return ReleaseArtifactService.GetVersions(product, os, architecture).ToProductVersionListResponse();
         }
         
+        
+        /// <summary>
+        /// Retrieves the artifact of the specified product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="os"></param>
+        /// <param name="architecture"></param>
+        /// <param name="version"></param>
+        /// <response code="200">There exists a product with the specified product name / arch / version / os.</response>
+        /// <response code="500">There exists no product with the specified product name / arch / version / os.</response>
+        /// <returns></returns>
         [AllowAnonymous]
         [HttpGet("download/{product}/{os}/{architecture}/{version}")]
+        [ProducesResponseType(typeof(byte), 200)]
+        [ProducesResponseType(typeof(string), 500)]
         public IActionResult  GetSpecificArtifact([Required] string product, [Required] string os, [Required] string architecture, string version)
         {
             var provider = new FileExtensionContentTypeProvider();
@@ -100,8 +169,19 @@ namespace ReleaseServer.WebApi.Controllers
             return new FileContentResult(response.Payload, contentType);
         }
         
+        /// <summary>
+        /// Retrieves the latest artifact of a specific product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="os"></param>
+        /// <param name="architecture"></param>
+        /// <response code="200">The specified product exists.</response>
+        /// <response code="500">The product is not available vor the specified platform (OS + arch)</response>
+        /// <returns></returns>
         [AllowAnonymous]
         [HttpGet("download/{product}/{os}/{architecture}/latest")]
+        [ProducesResponseType(typeof(IActionResult), 200)]
+        [ProducesResponseType(typeof(string), 500)]
         public IActionResult  GetLatestArtifact([Required] string product, [Required] string os, [Required] string architecture)
         {
             var provider = new FileExtensionContentTypeProvider();
@@ -125,13 +205,36 @@ namespace ReleaseServer.WebApi.Controllers
             return new FileContentResult(response.Payload, contentType);
         }
 
+        /// <summary>
+        /// Retrieves the newest version of a specific product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="os"></param>
+        /// <param name="architecture"></param>
+        /// <response code="200">The specified product exists.</response>
+        /// <response code="500">The product is not available for the specified platform (OS + HW architecture)
+        /// or the product with the specified product name does not exist</response>
+        /// <returns></returns>
         [AllowAnonymous]
         [HttpGet("latest/{product}/{os}/{architecture}")]
+        [ProducesResponseType(typeof(ProductVersionResponseModel), 200)]
+        [ProducesResponseType(typeof(string), 500)]
         public ProductVersionResponseModel GetLatestVersion([Required] string product, [Required] string os, [Required] string architecture)
         {
             return ReleaseArtifactService.GetLatestVersion(product, os, architecture).ToProductVersionResponse();
         }
         
+        /// <summary>
+        /// Deletes the specified product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="os"></param>
+        /// <param name="architecture"></param>
+        /// <param name="version"></param>
+        /// <response code="200">The specified product got deleted successfully.</response>
+        /// <response code="401">The user is not authorized (wrong credentials or missing auth header.</response>
+        /// <response code="500">There exists no product with the specified product name.</response>
+        /// <returns></returns>
         [HttpDelete("{product}/{os}/{architecture}/{version}")]
         public IActionResult DeleteSpecificArtifact ([Required] string product, [Required] string os, [Required] string architecture, [Required] string version)
         {
@@ -140,7 +243,18 @@ namespace ReleaseServer.WebApi.Controllers
             return Ok("artifact successfully deleted");
         }
         
+        /// <summary>
+        /// Deletes all products of a specific product name.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        /// <response code="200">All products of the specified product name got deleted successfully.</response>
+        /// <response code="401">The user is not authorized (wrong credentials or missing auth header.</response>
+        /// <response code="500">There exists no product with the specified product name.</response>
         [HttpDelete("{product}")]
+        [ProducesResponseType(typeof(IActionResult), 200)]
+        [ProducesResponseType(typeof(void), 401)]
+        [ProducesResponseType(typeof(void), 500)]
         public IActionResult DeleteProduct ([Required] string product)
         {
             ReleaseArtifactService.DeleteProduct(product);
@@ -148,7 +262,15 @@ namespace ReleaseServer.WebApi.Controllers
             return Ok("product successfully deleted");
         }
         
+        /// <summary>
+        /// Backups the whole artifact directory and retrieves it as a ZIP file.
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">The artifact directory backup was successful.</response>
+        /// <response code="401">The user is not authorized (wrong credentials or missing auth header.</response>
         [HttpGet("backup")]
+        [ProducesResponseType(typeof(IActionResult), 200)]
+        [ProducesResponseType(typeof(void), 401)]
         public FileStreamResult Backup()
         {
             var provider = new FileExtensionContentTypeProvider();
@@ -170,7 +292,16 @@ namespace ReleaseServer.WebApi.Controllers
             };
         }
 
+        /// <summary>
+        /// Restores the uploaded backup file.
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">The restore process was successful.</response>
+        /// <response code="401">The user is not authorized (wrong credentials or missing auth header.</response>
         [HttpPut("restore")]
+        [ProducesResponseType(typeof(IActionResult), 200)]
+        [ProducesResponseType(typeof(void), 401)]
+        
         public IActionResult Restore()
         {
             var payload = Request.Form.Files.FirstOrDefault();
