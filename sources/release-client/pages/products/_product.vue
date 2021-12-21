d4fbf44a9465dc824cddd23a435af2c3adb76496
Fabian Träger <f.traeger@traeger.de>
2021-12-21 11:24:27 +0100

    minor layout optimizations

--- sources/release-client/pages/products/_product.vue
+++ sources/release-client/pages/products/_product.vue
@@ -1,20 +1,25 @@
 <template>
   <Page :title="product.identifier" back-link="/products">
     <div class="flex gap-2">
-      <div class="flex flex-col gap-2 w-1/3">
-        <UiCard
+      <UiPane class="flex flex-col gap-2 w-1/3 overflow-y-auto">
+        <div
           v-for="release in product.releases"
           :key="release.version"
           :title="release.version"
-          class="hover:bg-gray-700 cursor-pointer"
+          class="rounded px-3 py-2 hover:bg-gray-700 hover:text-white cursor-pointer tabular-nums font-semibold flex justify-between"
           :class="[
-            isSelected(release) ? 'bg-gray-700' : ''
+            isSelected(release) ? 'bg-gray-700 text-white' : ''
           ]"
-          @click.native="select(release)"
+          @click="select(release)"
         >
-          {{ new Date(release.releaseDate).toLocaleDateString() }}
-        </UiCard>
-      </div>
+          <span>
+            v{{ release.version }}
+          </span>
+          <span>
+            [{{ new Date(release.releaseDate).toLocaleDateString() }}]
+          </span>
+        </div>
+      </UiPane>
       <div class="flex flex-col gap-2 w-2/3">
         <UiPane
           v-if="hasSelection"
