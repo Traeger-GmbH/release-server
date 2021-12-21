4d522d5f6ed85f32802fd3af949bbad3b4b1d3f6
Fabian Träger <f.traeger@traeger.de>
2021-12-21 11:05:40 +0100

    use max width to make adaptable to page width

--- sources/release-client/components/LinkCard.vue
+++ sources/release-client/components/LinkCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="transition-all ease-out duration-500 rounded w-96 h-32 bg-white hover:bg-gray-700 group shadow-xl border border-gray-300">
+  <div class="transition-all ease-out duration-500 rounded max-w-sm h-32 bg-white hover:bg-gray-700 group shadow-xl border border-gray-300">
     <NuxtLink :to="link" class="w-full h-full block">
       <div class="py-3 px-4 w-full h-full">
         <div class="w-full text-xl mb-3 pb-2 border-b-2 border-gray-700 border-green-500 text-green-500 font-semibold">
