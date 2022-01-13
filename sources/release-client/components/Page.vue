8b3cfb773c5b71614120ae978dfaf663151a0a0b
Fabian Träger <f.traeger@traeger.de>
2022-01-13 14:54:09 +0100

    fix layout to avoid product name wrapping

--- sources/release-client/components/Page.vue
+++ sources/release-client/components/Page.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="flex flex-col flex-grow flex-shrink min-h-0">
-    <div class="flex items-center flex-grow-0 flex-shrink py-5">
-      <h1 class="mr-5">{{title}}</h1>
-      <NuxtLink
-        v-if="backLink"
-        :to="backLink"
-        class="btn btn-green flex justify-center"
-      >
-        go back
-      </NuxtLink>
-      <div class="flex w-full">
+    <div class="flex flex-row items-center flex-grow-0 flex-shrink py-5">
+      <div class="flex-grow flex flex-row items-center">
+        <h1 class="mr-5">{{title}}</h1>
+        <NuxtLink
+          v-if="backLink"
+          :to="backLink"
+          class="btn btn-green flex justify-center flex-grow-0"
+        >
+          go back
+        </NuxtLink>
+      </div>
+      <div class="flex flex-grow">
         <slot name="header"></slot>
       </div>
     </div>
