3026c63c7d5bc429473b34b832cbd17b94f1bffb
Fabian Träger <f.traeger@traeger.de>
2022-01-13 15:52:50 +0100

    enable filtering on releases too

--- sources/release-client/components/ProductList.vue
+++ sources/release-client/components/ProductList.vue
@@ -6,14 +6,23 @@
     >
       <div class="flex flex-col w-1/3 flex-grow">
         <UiPane class="overflow-auto min-h-0 gap-2 flex flex-col flex-grow">
-          <div class="rounded flex flex-row items-center gap-2 font-semibold -mt-4 -mx-4 px-4 py-4 bg-gray-500 mb-2">
+          <div class="rounded flex flex-row items-center gap-2 font-semibold -mt-4 -mx-4 px-4 py-4 bg-gray-500 mb-2 relative">
             <input
               id="filter"
-              class="rounded px-3 py-2 flex-grow focus:outline-none focus:ring ring-green-500"
+              class="rounded px-3 py-2 flex-grow focus:outline-none focus:ring ring-green-500 font-medium"
               type="text"
               v-model="filter"
-              placeholder="Filter products"
-            />
+              placeholder="Filter"
+              @keyup.esc="filter = null"
+              autocomplete="off"
+            >
+            <button
+              v-if="filter && filter.length > 0"
+              class="absolute top-6 right-8 text-gray-500 font-semibold"
+              @click="filter = null"
+            >
+              x
+            </button>
           </div>
           <NuxtLink
             v-for="product in filteredProducts"
--- sources/release-client/components/ProductView.vue
+++ sources/release-client/components/ProductView.vue
@@ -2,8 +2,26 @@
   <div class="flex flex-grow flex-shrink gap-2 min-h-0">
     <UiPane class="flex flex-col gap-2 w-1/3 overflow-y-auto">
       <template v-if="product">
+        <div class="rounded flex flex-row items-center gap-2 font-semibold -mt-4 -mx-4 px-4 py-4 bg-gray-500 mb-2 relative">
+          <input
+            id="filter"
+            class="rounded px-3 py-2 flex-grow focus:outline-none focus:ring ring-green-500 font-medium"
+            type="text"
+            v-model="filter"
+            placeholder="Filter"
+            @keyup.esc="filter = null"
+            autocomplete="off"
+          >
+          <button
+            v-if="filter && filter.length > 0"
+            class="absolute top-6 right-8 text-gray-500 font-semibold"
+            @click="filter = null"
+          >
+            x
+          </button>
+        </div>
         <div
-          v-for="release in product.releases"
+          v-for="release in filteredReleases"
           :key="release.version"
           :title="release.version"
           class="rounded px-3 py-2 hover:bg-gray-700 hover:text-white cursor-pointer tabular-nums font-semibold flex justify-between"
@@ -65,7 +83,8 @@ export default {
     return {
       isLoading: false,
       product: null,
-      selectedRelease: null
+      selectedRelease: null,
+      filter: null
     };
   },
   async fetch () {
@@ -80,6 +99,25 @@ export default {
   computed: {
     hasSelection () {
       return this.selectedRelease !== null;
+    },
+    filteredReleases () {
+      if (this.filter !== null) {
+        return this.product.releases.filter(release => release.version.startsWith(this.trimmedFilter));
+      } else {
+        return this.product.releases;
+      }
+    },
+    trimmedFilter () {
+      if (this.filter !== null) {
+        const splitFilter = this.filter.split('v');
+        if (splitFilter.length > 1) {
+          return splitFilter[1];
+        } else {
+          return splitFilter[0];
+        }
+      } else {
+        return this.filter;
+      }
     }
   },
   methods: {
