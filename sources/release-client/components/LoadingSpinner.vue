b1159997ff6d30fc91ae2f915c5f9e9a9372e73f
Fabian Träger <f.traeger@traeger.de>
2022-01-13 15:18:26 +0100

    fix parameter types

--- sources/release-client/components/LoadingSpinner.vue
+++ sources/release-client/components/LoadingSpinner.vue
@@ -4,13 +4,13 @@
     v-show="show"
   >
     <VueSpinner
-      size="80"
-      speed="1.5"
-      spacing="25"
-      line-size="12"
+      :size="80"
+      :speed="1.5"
+      :spacing="25"
+      :line-size="12"
       line-fg-color="#10b981"
       :message="message"
-      font-size="20"
+      :font-size="20"
     />
   </div>
 </template>
