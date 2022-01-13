f951dfc9c740220f9a241b5a844f2fb6e416e0b3
Fabian Träger <f.traeger@traeger.de>
2022-01-13 14:53:44 +0100

    redesign ProductList component

--- sources/release-client/components/ProductList.vue
+++ sources/release-client/components/ProductList.vue
@@ -2,16 +2,30 @@
   <div class="flex flex-col gap-2 min-h-0 flex-grow">
     <div
       v-if="products && products.length > 0"
-      class="flex-shrink flex-grow flex flex-row min-h-0"
+      class="flex-shrink flex-grow flex flex-col min-h-0"
     >
-      <UiPane class="overflow-auto min-h-0 gap-2 flex flex-col w-1/3">
-        <LinkCard
-          v-for="product in products"
-          :key="product"
-          :title="product"
-          :link="'/products/' + product"
-        />
-      </UiPane>
+      <div class="flex flex-col w-1/3 flex-grow">
+        <div class="pb-2 flex flex-row items-center gap-2 font-semibold">
+          <input
+            id="filter"
+            class="bg-gray-100 rounded px-3 py-2 flex-grow"
+            type="text"
+            v-model="filter"
+            placeholder="Filter products"
+          />
+        </div>
+        <UiPane class="overflow-auto min-h-0 gap-2 flex flex-col flex-grow">
+          <NuxtLink
+            v-for="product in filteredProducts"
+            :key="product"
+            :title="product"
+            :to="'/products/' + product"
+            class="bg-gray-100 rounded px-3 py-2 font-semibold hover:bg-gray-700 hover:text-white"
+          >
+            {{ product }}
+          </NuxtLink>
+        </UiPane>
+      </div>
     </div>
     <div
       v-else
@@ -37,7 +51,8 @@ export default {
     return {
       selectedProduct: null,
       products: null,
-      isLoading: false
+      isLoading: false,
+      filter: null
     };
   },
   async fetch () {
@@ -51,6 +66,13 @@ export default {
   computed: {
     hasSelection () {
       return this.selectedProduct !== null;
+    },
+    filteredProducts () {
+      if (this.filter !== null) {
+        return this.products.filter(product => product.includes(this.filter));
+      } else {
+        return this.products;
+      }
     }
   },
   methods: {
