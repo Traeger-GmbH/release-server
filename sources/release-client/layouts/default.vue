8f4de701ab8ffc3dd34c2ab5f023276866aaed20
Fabian Träger <f.traeger@traeger.de>
2022-01-13 13:43:54 +0100

    add margin

--- sources/release-client/layouts/default.vue
+++ sources/release-client/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen mx-2 flex flex-col">
-    <Navigation class="flex-grow-0 flex-shrink" />
+    <Navigation class="flex-grow-0 flex-shrink mt-1" />
     <div class="container mx-auto flex-grow flex-shrink flex flex-col min-h-0">
       <Nuxt />
     </div>
